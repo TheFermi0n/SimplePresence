b31f1e424358d3dd1adb462ffc8f6215cc93efd7
TheFermi0n <beetle-06-arrays@icloud.com>
2024-08-21 15:28:49 +0900

    Hoo Lee Sheet

--- scripts/custom/main.js
+++ scripts/custom/main.js
@@ -1,129 +1,147 @@
 /* eslint-disable no-console */
 
-const {
-  app,
-  BrowserWindow
-} = require('electron');
-const path = require('path');
-const url = require('url');
-const DiscordRPC = require('discord-rpc');
-const config = require('../../config.json');
-const fs = require('fs');
-const parse = require('parse-duration')
-const moment = require('moment')
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+const url = require("url");
+const DiscordRPC = require("discord-rpc");
+const config = require("../../config.json");
+// const fs = require("fs");
+const parse = require("parse-duration");
+const moment = require("moment");
 
 if (config.defaultText || config.imageKeys) {
-  console.log('ERROR: The config system has been altered since the last update. Please check config.json.example and update your config.\n')
+  console.log(
+    "ERROR: The config system has been altered since the last update. Please check config.json.example and update your config.\n",
+  );
   return app.quit();
 }
 
 const ClientId = config.clientID;
-var openTimestamp
+var openTimestamp;
 let mainWindow;
 
 function createWindow() {
-  var width = 500 //320
-  var height = 420 //500
+  var width = 450; //320
+  var height = 500; //500
   mainWindow = new BrowserWindow({
     width: width,
     height: height,
-    resizable: false,
-    titleBarStyle: 'hidden',
-    vibrancy: 'dark',
-    hasShadow: false,
+    resizable: true,
+    titleBarStyle: "hidden",
+    vibrancy: "window",
+    hasShadow: true,
     frame: false,
     show: false,
     webPreferences: {
-      nodeIntegration: true
-    }
+      nodeIntegration: true, // Enable nodeIntegration
+      contextIsolation: false, // Disable contextIsolation
+    },
   });
 
   if (config.imageConfig.showButton == true) {
-    var height = height + 40
+    var height = height + 40;
     mainWindow.setSize(width, height);
   }
 
-  if (config.timeConfig.timeType !== 'none') {
-    var height = height + 60
+  if (config.timeConfig.timeType !== "none") {
+    var height = height + 60;
     mainWindow.setSize(width, height);
   }
 
-  mainWindow.on('ready-to-show', () => {
-    mainWindow.show()
-  })
+  mainWindow.on("ready-to-show", () => {
+    mainWindow.show();
+  });
 
-  mainWindow.loadURL(url.format({
-    pathname: path.join(__dirname, 'index.html'),
-    protocol: 'file:',
-    slashes: true,
-  }));
+  mainWindow.loadURL(
+    url.format({
+      pathname: path.join(__dirname, "index.html"),
+      protocol: "file:",
+      slashes: true,
+    }),
+  );
 
-  mainWindow.on('closed', () => {
+  mainWindow.on("closed", () => {
     mainWindow = null;
   });
 }
 
-function getDuration() {
+// function getDuration() {}
 
-}
+app.on("ready", createWindow);
 
-app.on('ready', createWindow);
-
-app.on('window-all-closed', () => {
+app.on("window-all-closed", () => {
   app.quit();
 });
 
-app.on('activate', () => {
-  if (mainWindow === null)
-    createWindow();
+app.on("activate", () => {
+  if (mainWindow === null) createWindow();
 });
 
 DiscordRPC.register(ClientId);
 
 const rpc = new DiscordRPC.Client({
-  transport: 'ipc'
+  transport: "ipc",
 });
 
 async function setActivity() {
-  if (!rpc || !mainWindow)
-    return;
+  if (!rpc || !mainWindow) return;
 
-  var ltext = await mainWindow.webContents.executeJavaScript('var text = "textContent" in document.body ? "textContent" : "innerText";document.getElementById("ltext")[text];')
-  var details = await mainWindow.webContents.executeJavaScript('var text = "textContent" in document.body ? "textContent" : "innerText";document.getElementById("details")[text];')
-  var state = await mainWindow.webContents.executeJavaScript('var text = "textContent" in document.body ? "textContent" : "innerText";document.getElementById("state")[text];')
-  var stext = await mainWindow.webContents.executeJavaScript('var text = "textContent" in document.body ? "textContent" : "innerText";document.getElementById("stext")[text];')
-  var lkey = await mainWindow.webContents.executeJavaScript('var text = "textContent" in document.body ? "textContent" : "innerText";document.getElementById("lkey")[text];')
-  var skey = await mainWindow.webContents.executeJavaScript('var text = "textContent" in document.body ? "textContent" : "innerText";document.getElementById("skey")[text];')
+  var ltext = await mainWindow.webContents.executeJavaScript(
+    'var text = "textContent" in document.body ? "textContent" : "innerText";document.getElementById("ltext")[text];',
+  );
+  var details = await mainWindow.webContents.executeJavaScript(
+    'var text = "textContent" in document.body ? "textContent" : "innerText";document.getElementById("details")[text];',
+  );
+  var state = await mainWindow.webContents.executeJavaScript(
+    'var text = "textContent" in document.body ? "textContent" : "innerText";document.getElementById("state")[text];',
+  );
+  var stext = await mainWindow.webContents.executeJavaScript(
+    'var text = "textContent" in document.body ? "textContent" : "innerText";document.getElementById("stext")[text];',
+  );
+  var lkey = await mainWindow.webContents.executeJavaScript(
+    'var text = "textContent" in document.body ? "textContent" : "innerText";document.getElementById("lkey")[text];',
+  );
+  var skey = await mainWindow.webContents.executeJavaScript(
+    'var text = "textContent" in document.body ? "textContent" : "innerText";document.getElementById("skey")[text];',
+  );
 
   var activity = {
     details: details,
     state: state,
     largeImageKey: lkey,
     largeImageText: ltext,
-    instance: false
-  }
+    instance: false,
+  };
 
-  if (skey !== 'none') {
-    activity.smallImageKey = skey
-    activity.smallImageText = stext
+  if (skey !== "none") {
+    activity.smallImageKey = skey;
+    activity.smallImageText = stext;
   }
 
   if (!openTimestamp) {
     openTimestamp = new Date();
   }
 
-  if (config.timeConfig.timeType == 'start') {
-    activity.startTimestamp = moment(openTimestamp).add(parse('-' + config.timeConfig.whatTime), 'ms').toDate();
-  } else if (config.timeConfig.timeType == 'end') {
-    activity.endTimestamp = moment(openTimestamp).add(parse(config.timeConfig.whatTime), 'ms').toDate();
-  } else if (config.timeConfig.timeType == 'both') {
-    activity.startTimestamp = moment(openTimestamp).add(parse('0s'), 'ms').toDate();
-    activity.endTimestamp = moment(openTimestamp).add(parse(config.timeConfig.whatTime), 'ms').toDate();
+  if (config.timeConfig.timeType == "start") {
+    activity.startTimestamp = moment(openTimestamp)
+      .add(parse("-" + config.timeConfig.whatTime), "ms")
+      .toDate();
+  } else if (config.timeConfig.timeType == "end") {
+    activity.endTimestamp = moment(openTimestamp)
+      .add(parse(config.timeConfig.whatTime), "ms")
+      .toDate();
+  } else if (config.timeConfig.timeType == "both") {
+    activity.startTimestamp = moment(openTimestamp)
+      .add(parse("0s"), "ms")
+      .toDate();
+    activity.endTimestamp = moment(openTimestamp)
+      .add(parse(config.timeConfig.whatTime), "ms")
+      .toDate();
   }
   rpc.setActivity(activity);
 }
 
-rpc.on('ready', () => {
+rpc.on("ready", () => {
   setActivity();
 
   setInterval(() => {
@@ -131,4 +149,4 @@ rpc.on('ready', () => {
   }, 15e3);
 });
 
-rpc.login({clientId: ClientId}).catch(console.error);
+rpc.login({ clientId: ClientId }).catch(console.error);
--- scripts/custom/renderer.js
+++ scripts/custom/renderer.js
@@ -1,38 +1,42 @@
-const {
-  webFrame
-} = require('electron');
-const {
-  remote
-} = require('electron')
-const mainProcess = remote.require('./main.js');
-const os = require('os');
-if (os.type() !== 'Darwin') {
-  document.body.style.backgroundColor = '#4C4C4C'
-}
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("DOM fully loaded and parsed");
 
-webFrame.setVisualZoomLevelLimits(1, 1);
+  const { webFrame } = require("electron");
+  const os = require("os");
 
-const config = require('../../config.json')
-console.log(config.textConfig.details)
-console.log(config.textConfig.state)
-console.log(config.imageConfig.smallText)
-var text = "textContent" in document.body ? "textContent" : "innerText";
-document.getElementById('details')[text] = config.textConfig.details
-document.getElementById('state')[text] = config.textConfig.state
-document.getElementById('stext')[text] = config.imageConfig.smallText
-document.getElementById('ltext')[text] = config.imageConfig.largeText
-document.getElementById('skey')[text] = config.imageConfig.smallKey
-document.getElementById('lkey')[text] = config.imageConfig.largeKey
-if (config.imageConfig.showButton == false) {
-  document.getElementById('button').style.display = 'none'
-}
-if (config.timeConfig.timeType !== 'none') {
-  document.getElementById('time')[text] = config.timeConfig.whatTime
-} else {
-  document.getElementById('divtime').style.display = 'none'
-}
+  try {
+    const config = require("../../config.json");
+    console.log("Config loaded:", config);
 
-function upload() {
-  var open = require("open");
-  open('https://canary.discordapp.com/developers/applications/me/' + config.clientID.toString());
-}
+    if (os.type() !== "Darwin") {
+      document.body.style.backgroundColor = "#4C4C4C";
+    }
+
+    webFrame.setVisualZoomLevelLimits(1, 1);
+
+    console.log(config.textConfig.details);
+    console.log(config.textConfig.state);
+    console.log(config.imageConfig.smallText);
+    var text = "textContent" in document.body ? "textContent" : "innerText";
+    document.getElementById("details")[text] = config.textConfig.details;
+    document.getElementById("state")[text] = config.textConfig.state;
+    document.getElementById("stext")[text] = config.imageConfig.smallText;
+    document.getElementById("ltext")[text] = config.imageConfig.largeText;
+    document.getElementById("skey")[text] = config.imageConfig.smallKey;
+    document.getElementById("lkey")[text] = config.imageConfig.largeKey;
+
+    console.log("UI elements updated");
+
+    if (!config.imageConfig.showButton) {
+      document.getElementById("button").style.display = "none";
+    }
+
+    if (config.timeConfig.timeType !== "none") {
+      document.getElementById("time")[text] = config.timeConfig.whatTime;
+    } else {
+      document.getElementById("divtime").style.display = "none";
+    }
+  } catch (error) {
+    console.error("Failed to load config or update UI:", error);
+  }
+});
